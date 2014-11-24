0b917185058f58f15193cd08e96f55e94d451e5e
OliverD <oliver312@gmail.com>
2014-11-23 23:36:28 -0400

    Initial commit

---
+++ main.rb
@@ -0,0 +1,50 @@
+require 'sinatra'
+require 'data_mapper'
+require 'json'
+require 'dm-core'
+require 'dm-tags'
+
+DataMapper.setup :default, 'sqlite3::memory:'
+
+class Posts
+	include DataMapper::Resource
+
+	property :id, Serial
+	property :title, Text, :required => true
+	property :autor, Text
+	property :description, Text
+	has_tags_on  :tags 
+end
+
+DataMapper.finalize.auto_upgrade!
+
+file = File.read('posts.json')
+_posts = JSON.parse(file)
+
+_posts['posts'].each do |post| 
+
+	p = Posts.create(
+		:id => post['id'],
+		:title => post['title'],
+		:autor => post['autor'],
+		:description => post['description']) 
+	p.tag_list = post['tags']
+	puts p.tag_list
+	p.save
+
+end
+
+get '/' do
+
+	@posts = Posts.all.map do|elem| 
+		elem.description.split[1..10].join(" ")
+		elem
+	end
+	erb :index
+end
+
+get 'tags/:id' do
+	@tag = Note.get params[:id]
+	@title = "The tag you searched for is ##{params[:id]}"
+	erb :tags
+end
