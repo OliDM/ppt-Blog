0bdac568d9180c72f97ed48aa64d46f2f5b539c3
OliverD <oliver312@gmail.com>
2014-11-26 23:39:43 -0400

    replacing should method for the expect function.

--- spec/routes_spec.rb
+++ spec/routes_spec.rb
@@ -1,26 +1,27 @@
 require File.expand_path '../spec_helper.rb', __FILE__
-
+require 'debugger'
 describe "My Sinatra Application" do
   it "should allow accessing the home page" do
     get '/'
-    last_response.should be_ok
+    debugger
+    expect(last_response.ok?).to eq(true)
   end
 
   it "accesing the post by id route" do
     get '/post/1'
-    last_response.should be_ok
+    expect(last_response.ok?).to eq(true)
   end
 
 
   it "accesing the author by name route" do
     get '/author/Pixelated'
-    last_response.should be_ok
+    expect(last_response.ok?).to eq(true)
   end
 
 
   it "accesing the posts by tag name route" do
     get '/tags/fitness'
-    last_response.should be_ok
+    expect(last_response.ok?).to eq(true)
   end
 
 end
